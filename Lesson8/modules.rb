87f27bc1bc49078b17097613031dc9a7d8816ca0
eduard.hroza <eduard.hroza@gmail.com>
2023-09-08 16:46:35 +0400

    Rubocop autocorrection.

---
+++ Lesson8/modules.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Manufacturer
+  attr_accessor :manufacturer
+end
+
+module InstanceCounter
+  def self.included(base)
+    base.extend(ClassMethods)
+    base.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+    attr_accessor :instances
+
+    def instances
+      @instances ||= 0
+    end
+  end
+
+  module InstanceMethods
+    private
+
+    def register_instance
+      self.class.instances += 1
+    end
+  end
+
+  module Validity
+    def valid?
+      validate!
+      true
+    rescue StandardError
+      false
+    end
+  end
+end
---
+++ Lesson8/route.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require_relative 'modules'
+
+class Route
+  attr_accessor :stations
+
+  include InstanceCounter
+  NUMBER_FORMAT = /^[A-Za-z0-9]{3}-?[A-Za-z0-9]{2}$/.freeze
+
+  def initialize(first_station, last_station)
+    @stations = [first_station, last_station]
+  end
+
+  def validate!
+    errors = []
+    errors << "Name can't be empty." if [first_station, last_station].any?(&:nil? || :empty?)
+    errors << 'Invalid format.' unless first_station =~ NUMBER_FORMAT && last_station =~ NUMBER_FORMAT
+    raise errors.join('.') unless errors.empty?
+  end
+
+  def add_intermediate_station(station)
+    @stations.insert(-2, station)
+  end
+
+  def remove_intermediate_station(station)
+    @stations.delete(station)
+  end
+
+  def display_stations
+    puts 'Stations:'
+    @stations.each { |station| puts station.name }
+  end
+
+  def station_list
+    @stations
+  end
+end
